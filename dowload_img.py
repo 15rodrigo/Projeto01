2ab5a26ba0d21fabca1a78e1b21e973ad048da69
Rodrigo Macedo <rmlima93@outlook.com>
2022-11-11 15:05:29 -0300

    atualização

--- dowload_img.py
+++ dowload_img.py
@@ -4,10 +4,4 @@ from bs4 import BeautifulSoup
 page = requests.get("https://produto.mercadolivre.com.br/MLB-2901821860")
 souped = BeautifulSoup(page.content, "html.parser")
 imgs = souped.find_all("img")
-imgs = imgs[13:-1]
-
-for img in imgs:
-    imglink = img.attrs.get("data-zoom")
-    
-    print(imglink)
-
+print(imgs)
