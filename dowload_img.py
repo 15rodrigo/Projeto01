8c2e99b38719f45c215deee905ff15567ae74832
Rodrigo Macedo <rmlima93@outlook.com>
2022-11-11 15:13:08 -0300

    k

--- dowload_img.py
+++ dowload_img.py
@@ -4,4 +4,8 @@ from bs4 import BeautifulSoup
 page = requests.get("https://produto.mercadolivre.com.br/MLB-2901821860")
 souped = BeautifulSoup(page.content, "html.parser")
 imgs = souped.find_all("img")
-print(imgs)
+imgs = imgs[13:-1]
+
+for img in imgs:
+    imglink = img.attrs.get("data-zoom")
+    print(imglink)
