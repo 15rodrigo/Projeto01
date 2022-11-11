8d4a45419416b2c92c5caad99547a8ca33f7f51d
Rodrigo Macedo <rmlima93@outlook.com>
2022-11-11 16:41:19 -0300

    tewste

--- dowload_img.py
+++ dowload_img.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 page = requests.get("https://produto.mercadolivre.com.br/MLB-2901821860")
 souped = BeautifulSoup(page.content, "html.parser")
 imgs = souped.find_all("img")
 imgs = imgs[13:-1]
-
-for img in imgs:
+for img in tqdm(imgs):
     imglink = img.attrs.get("data-zoom")
-    print(imglink)
+    imagem = requests.get(imglink).content
+    filename = r"fotos" + imglink[imglink.rfind("/"):]
+    with open (filename, "wb") as file:
+        file.write(imagem)
